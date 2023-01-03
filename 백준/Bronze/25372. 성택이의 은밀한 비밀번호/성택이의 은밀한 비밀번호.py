f7fe18307b6ef471014266add5de12196d97a3f5
lemon-lime-honey <121420533+lemon-lime-honey@users.noreply.github.com>
2023-01-03 14:27:48 +0900

    [Bronze V] Title: 성택이의 은밀한 비밀번호, Time: 148 ms, Memory: 30616 KB -BaekjoonHub

---
+++ 백준/Bronze/25372. 성택이의 은밀한 비밀번호/성택이의 은밀한 비밀번호.py
@@ -0,0 +1,8 @@
+num = int(input())
+for i in range(num):
+    password = input()
+    length = len(password)
+    if ((length >= 6) & (length <= 9)):
+        print("yes")
+    else:
+        print("no")
