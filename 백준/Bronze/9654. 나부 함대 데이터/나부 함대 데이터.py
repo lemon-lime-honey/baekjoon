2012f0850e481a02df6257e8082f78c729bc1b5c
lemon-lime-honey <121420533+lemon-lime-honey@users.noreply.github.com>
2023-01-03 14:21:11 +0900

    [Bronze V] Title: 나부 함대 데이터, Time: 36 ms, Memory: 30616 KB -BaekjoonHub

---
+++ 백준/Bronze/9654. 나부 함대 데이터/나부 함대 데이터.py
@@ -0,0 +1,6 @@
+print("SHIP NAME" + " " * 6 + "CLASS" + " " * 10 + "DEPLOYMENT" + " " * 1 + "IN SERVICE")
+print("N2 Bomber" + " " * 6 +"Heavy Fighter" + " " * 2 + "Limited" + " " * 4 + "21" + " " * 8)
+print("J-Type 327" + " " * 5 + "Light Combat" + " " * 3 + "Unlimited" + " " * 2 + "1" + " " * 9)
+print("NX Cruiser" + " " * 5 + "Medium Fighter" + " " * 1 + "Limited" + " " * 4 + "18" + " " * 8)
+print("N1 Starfighter" + " " * 1 + "Medium Fighter" + " " * 1 + "Unlimited" + " " * 2 + "25" + " " * 8)
+print("Royal Cruiser" + " " * 2 + "Light Combat" + " " * 3 + "Limited" + " " * 4 + "4" + " " * 9)
