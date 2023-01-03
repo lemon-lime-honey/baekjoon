f5b4a7156e139604c5450397810242d1c1204679
lemon-lime-honey <121420533+lemon-lime-honey@users.noreply.github.com>
2023-01-03 14:00:28 +0900

    [Bronze II] Title: 음계, Time: 40 ms, Memory: 30616 KB -BaekjoonHub

---
+++ 백준/Bronze/2920. 음계/음계.py
@@ -0,0 +1,11 @@
+asc = '1 2 3 4 5 6 7 8'
+desc = '8 7 6 5 4 3 2 1'
+
+ipt = input()
+
+if (ipt == asc):
+    print("ascending")
+elif (ipt == desc):
+    print("descending")
+else:
+    print("mixed")
