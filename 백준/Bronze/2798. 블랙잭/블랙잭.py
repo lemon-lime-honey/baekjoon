25684012b6683b92adaf51b0fb9552c93a7e1065
lemon-lime-honey <121420533+lemon-lime-honey@users.noreply.github.com>
2023-01-03 13:59:46 +0900

    [Bronze II] Title: 블랙잭, Time: 160 ms, Memory: 28776 KB -BaekjoonHub

---
+++ 백준/Bronze/2798. 블랙잭/블랙잭.py
@@ -0,0 +1,14 @@
+n, m = map(int, input().split())
+card = list(map(int, input().split()))
+result = 0
+
+for i in range(0, n):
+    for j in range(i+1, n):
+        for k in range(j+1, n):
+            if card[i] + card[j] + card[k] <= m:
+                result = max(result, card[i] + card[j] + card[k])
+            else:
+                continue
+
+print(result)
+                
