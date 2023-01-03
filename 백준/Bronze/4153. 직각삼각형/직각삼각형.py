40236f051d8ded87141f920cf27a303319980b2c
lemon-lime-honey <121420533+lemon-lime-honey@users.noreply.github.com>
2023-01-03 14:40:13 +0900

    [Bronze III] Title: 직각삼각형, Time: 76 ms, Memory: 28776 KB -BaekjoonHub

---
+++ 백준/Bronze/4153. 직각삼각형/직각삼각형.py
@@ -0,0 +1,23 @@
+while (1):
+    a, b, c = map(int, input().split())
+
+    if ((a==0) and (b==0) and (c==0)):
+        break
+
+    maxi = max(a, b, c)
+
+    if (maxi == a):
+        if (a**2 == (b**2 + c**2)):
+            print('right')
+        else:
+            print('wrong')
+    elif (maxi == b):
+        if (b**2 == (a**2 + c**2)):
+            print('right')
+        else:
+            print('wrong')
+    else:
+        if (c**2 == (a**2 + b**2)):
+            print('right')
+        else:
+            print('wrong')
