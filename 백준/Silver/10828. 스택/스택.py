26e471aeb9629fbddd4ae6b36768fe9967f75a9e
lemon-lime-honey <121420533+lemon-lime-honey@users.noreply.github.com>
2023-01-03 14:24:00 +0900

    [Silver IV] Title: 스택, Time: 72 ms, Memory: 34092 KB -BaekjoonHub

---
+++ 백준/Silver/10828. 스택/스택.py
@@ -0,0 +1,30 @@
+import sys
+from collections import deque
+
+n = int(sys.stdin.readline())
+result = deque()
+
+for i in range(n):
+    command = sys.stdin.readline().split()
+    try:
+        command[1] = int(command[1])
+        if (command[0] == 'push'):
+            result.append(command[1])
+    except:
+        if (command[0] == 'pop'):
+            try:
+                print(result.pop())
+            except:
+                print(-1)
+        elif (command[0] == 'size'):
+            print(len(result))
+        elif (command[0] == 'empty'):
+            if len(result):
+                print(0)
+            else:
+                print(1)
+        elif (command[0] == 'top'):
+            try:
+                print(result[-1])
+            except:
+                print(-1)
