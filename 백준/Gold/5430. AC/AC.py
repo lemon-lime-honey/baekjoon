1d0f3c7f053d493feeaf9c5d07ec65bf058d9bb0
lemon-lime-honey <121420533+lemon-lime-honey@users.noreply.github.com>
2023-01-09 15:51:47 +0900

    [Gold V] Title: AC, Time: 232 ms, Memory: 43060 KB -BaekjoonHub

---
+++ 백준/Gold/5430. AC/AC.py
@@ -0,0 +1,36 @@
+import sys
+from collections import deque
+
+t = int(sys.stdin.readline())
+
+for i in range(t):
+    p = sys.stdin.readline().strip()
+    n = int(sys.stdin.readline())
+    temp = sys.stdin.readline().strip().replace('[', '').replace(']', '')
+    flag = 0
+    try:
+        x = deque(map(str, temp.split(',')))
+    except:
+        flag = 1
+    
+    rev = False
+    for command in p:
+        if command == 'R':
+            rev = not rev
+        elif command == 'D':
+            if (len(x) == 0) + (n == 0):
+                flag = 1
+                break
+            else:
+                if rev:
+                    x.pop()
+                else:
+                    x.popleft()
+
+    if flag == 1:
+        print('error')
+    else:
+        if rev:
+            print(f"[{','.join(list(x)[::-1])}]")
+        else:
+            print(f"[{','.join(list(x))}]")
