2efcc9e9b0014516abf487543071493f391d8afd
lemon-lime-honey <121420533+lemon-lime-honey@users.noreply.github.com>
2023-01-17 23:03:03 +0900

    [Bronze V] Title: Serca, Time: 440 ms, Memory: 30616 KB -BaekjoonHub

---
+++ 백준/Bronze/26766. Serca/Serca.py
@@ -0,0 +1,16 @@
+def heart():
+    print(" @@@   @@@ ")
+    print("@   @ @   @")
+    print("@    @    @")
+    print("@         @")
+    print(" @       @ ")
+    print("  @     @  ")
+    print("   @   @   ")
+    print("    @ @    ")
+    print("     @     ")
+
+num = int(input())
+cnt = 0
+while (cnt != num):
+    cnt += 1
+    heart()
