79a053ed39950528916e6d53afad2d1c7c04125e
lemon-lime-honey <121420533+lemon-lime-honey@users.noreply.github.com>
2023-01-18 11:02:46 +0900

    [Bronze II] Title: 문자열 반복, Time: 36 ms, Memory: 30616 KB -BaekjoonHub

--- 백준/Bronze/2675. 문자열 반복/문자열 반복.py
+++ 백준/Bronze/2675. 문자열 반복/문자열 반복.py
@@ -1,20 +1,7 @@
 t = int(input())
-repeat = 0
-lst = list()
-let = ""
-temp = ""
-total = list()
 
-for i in range(0, t):
-    repeat, let = list(map(str, input().split()))
-    repeat = int(repeat)
-    lst = list(let)
-    for j in range(0, len(let)):
-        temp += lst[j]*repeat
-    total.append(temp)
-    let = ""
-    lst = list()
-    temp = ""
-
-for i in total:
-    print(i)
+for i in range(t):
+    r, s = map(str, input().split())
+    for letter in s:
+        print(letter * int(r), end = '')
+    print()
