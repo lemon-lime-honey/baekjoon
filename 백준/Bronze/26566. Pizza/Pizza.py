3edb5ff5a0a501b3d38974fae9fd71957e214244
lemon-lime-honey <121420533+lemon-lime-honey@users.noreply.github.com>
2023-01-22 14:21:37 +0900

    [Bronze IV] Title: Pizza, Time: 36 ms, Memory: 32540 KB -BaekjoonHub

---
+++ 백준/Bronze/26566. Pizza/Pizza.py
@@ -0,0 +1,14 @@
+from math import pi
+
+n = int(input())
+
+for i in range(n):
+    a1, p1 = map(int, input().split())
+    r1, p2 = map(int, input().split())
+    slice = a1 / p1
+    whole = (pi * r1 ** 2) / p2
+    
+    if slice < whole:
+        print('Whole pizza')
+    else:
+        print('Slice of pizza')
