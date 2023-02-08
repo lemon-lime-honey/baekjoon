130df7d6ce89afce4064b019c6cf2d69fe1810a5
B. Kim <121420533+lemon-lime-honey@users.noreply.github.com>
2023-02-08 20:42:28 +0900

    [Silver II] Title: 알고리즘 수업 - 깊이 우선 탐색 1, Time: 588 ms, Memory: 68912 KB -BaekjoonHub

---
+++ 백준/Silver/24479. 알고리즘 수업 － 깊이 우선 탐색 1/알고리즘 수업 － 깊이 우선 탐색 1.py
@@ -0,0 +1,32 @@
+import sys
+
+sys.setrecursionlimit(10 ** 6)
+
+def dfs(s):
+    global cnt
+    cnt += 1
+    visited[s] = True
+    result[s] = cnt
+    for point in line[s]:
+        if not visited[point]:
+            dfs(point)
+
+n, m, r = map(int, sys.stdin.readline().split())
+line = [[]for i in range(n + 1)]
+visited = [True] + [False] * n
+result = [0] * (n + 1)
+cnt = 0
+
+for i in range(m):
+    u, v = map(int, sys.stdin.readline().split())
+    line[u].append(v)
+    line[v].append(u)
+
+for i in range(n + 1):
+    line[i].sort()
+
+dfs(r)
+
+for i in range(1, n + 1):
+    print(result[i])
+
