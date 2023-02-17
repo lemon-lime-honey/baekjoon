6f2e8a646e75134bb3258aeb82314c1e3268f18d
B. Kim <121420533+lemon-lime-honey@users.noreply.github.com>
2023-02-17 15:35:54 +0900

    [level 2] Title: 루시와 엘라 찾기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/lv2/59046. 루시와 엘라 찾기/루시와 엘라 찾기.sql
+++ 프로그래머스/lv2/59046. 루시와 엘라 찾기/루시와 엘라 찾기.sql
@@ -3,4 +3,4 @@ SELECT
     name, 
     sex_upon_intake
 FROM animal_ins
-WHERE name IN ('Lucy', 'Ella', 'Pickle', 'Rogan', 'Sabrina', 'Mitty')
+WHERE name IN ('Lucy', 'Ella', 'Pickle', 'Rogan', 'Sabrina', 'Mitty');
