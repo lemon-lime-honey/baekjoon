9bc9f00b7b7779ef7654fa1fece5f3844eb68d1b
B. Kim <121420533+lemon-lime-honey@users.noreply.github.com>
2023-02-17 15:01:15 +0900

    [level 2] Title: 3월에 태어난 여성 회원 목록 출력하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/lv2/131120. 3월에 태어난 여성 회원 목록 출력하기/3월에 태어난 여성 회원 목록 출력하기.sql
+++ 프로그래머스/lv2/131120. 3월에 태어난 여성 회원 목록 출력하기/3월에 태어난 여성 회원 목록 출력하기.sql
@@ -2,7 +2,7 @@ SELECT
     member_id, 
     member_name, 
     gender, 
-    DATE_FORMAT(date_of_birth, '%Y-%m-%d')
+    DATE_FORMAT(date_of_birth, '%Y-%m-%d') AS 'date_of_birth'
 FROM member_profile
 WHERE
     MONTH(date_of_birth) = '3'
