978ed8d03ab57bae4bc46dad6772bae3ece399e3
B. Kim <121420533+lemon-lime-honey@users.noreply.github.com>
2023-05-17 19:50:46 +0900

    [Gold IV] Title: 최소 스패닝 트리, Time: 412 ms, Memory: 131088 KB -BaekjoonHub

---
+++ 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.py
@@ -0,0 +1,30 @@
+import heapq, sys
+input = sys.stdin.readline
+
+
+def find(p):
+    if p != chk[p]:
+        chk[p] = find(chk[p])
+    return chk[p]
+
+
+def union(p1, p2):
+    p1, p2 = find(p1), find(p2)
+
+    if p1 == p2: return
+    if p1 < p2: chk[p2] = p1
+    else: chk[p1] = p2
+
+
+v, e = map(int, input().split())
+graph = [list(map(int, input().split())) for i in range(e)]
+graph.sort(key=lambda x:x[2])
+chk = [i for i in range(v + 1)]
+result = 0
+
+for a, b, c in graph:
+    if find(a) != find(b):
+        union(a, b)
+        result += c
+
+print(result)
