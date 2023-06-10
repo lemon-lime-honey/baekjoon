ab10c8a98e824c0448fc982baa72fd36b9a507f1
B. Kim <121420533+lemon-lime-honey@users.noreply.github.com>
2023-06-10 10:49:14 +0900

    [Gold III] Title: 내리막 길, Time: 264 ms, Memory: 151400 KB -BaekjoonHub

---
+++ 백준/Gold/1520. 내리막 길/내리막 길.py
@@ -0,0 +1,27 @@
+import sys
+input = sys.stdin.readline
+
+dr = [0, 0, 1, -1]
+dc = [1, -1, 0, 0]
+
+
+def dfs(r, c):
+    if r == m - 1 and c == n - 1:
+        return 1
+
+    if dp[r][c] == -1:
+        dp[r][c] = 0
+        for i in range(4):
+            nr, nc = r + dr[i], c + dc[i]
+            if (0 <= nr < m) and (0 <= nc < n):
+                if graph[nr][nc] < graph[r][c]:
+                    dp[r][c] += dfs(nr, nc)
+
+    return dp[r][c]
+
+
+m, n = map(int, input().split())
+graph = [list(map(int, input().split())) for i in range(m)]
+dp = [[-1 for i in range(n)] for j in range(m)]
+
+print(dfs(0, 0))
