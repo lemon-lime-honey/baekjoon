c42b4b22f36db4c6cbad8d12685a216b88897d8e
B. Kim <121420533+lemon-lime-honey@users.noreply.github.com>
2023-08-28 09:56:19 +0900

    [Silver I] Title: 가장 큰 감소 부분 수열, Time: 132 ms, Memory: 114488 KB -BaekjoonHub

---
+++ 백준/Silver/17216. 가장 큰 감소 부분 수열/가장 큰 감소 부분 수열.py
@@ -0,0 +1,10 @@
+n = int(input())
+numbers = list(map(int, input().split()))
+dp = numbers[:]
+
+for i in range(n):
+    for j in range(i):
+        if numbers[i] < numbers[j]:
+            dp[i] = max(dp[i], dp[j] + numbers[i])
+
+print(max(dp))
