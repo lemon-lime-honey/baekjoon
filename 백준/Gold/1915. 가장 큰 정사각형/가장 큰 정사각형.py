48ca65efbd05e7b88c3cefa1646309dbba72d2e1
B. Kim <121420533+lemon-lime-honey@users.noreply.github.com>
2023-09-24 17:45:11 +0900

    [Gold IV] Title: 가장 큰 정사각형, Time: 220 ms, Memory: 124124 KB -BaekjoonHub

---
+++ 백준/Gold/1915. 가장 큰 정사각형/가장 큰 정사각형.py
@@ -0,0 +1,12 @@
+import sys
+input = sys.stdin.readline
+
+n, m = map(int, input().split())
+graph = [list(map(int, list(input().rstrip()))) for i in range(n)]
+
+for i in range(1, n):
+    for j in range(1, m):
+        if graph[i][j] == 1:
+            graph[i][j] += min(graph[i - 1][j - 1], graph[i - 1][j], graph[i][j - 1])
+
+print(max(map(max, graph)) ** 2)
