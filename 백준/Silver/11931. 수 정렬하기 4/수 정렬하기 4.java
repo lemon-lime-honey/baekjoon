fec49da5663588f44217b9cfb08c69f215f4f05c
B. Kim <121420533+lemon-lime-honey@users.noreply.github.com>
2023-11-13 11:05:01 +0900

    [Silver V] Title: 수 정렬하기 4, Time: 1816 ms, Memory: 160396 KB -BaekjoonHub

---
+++ 백준/Silver/11931. 수 정렬하기 4/수 정렬하기 4.java
@@ -0,0 +1,26 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Collections;
+import java.util.PriorityQueue;
+
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
+        StringBuilder sb = new StringBuilder();
+        int n = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < n; i++) {
+            pq.add(Integer.parseInt(br.readLine()));
+        }
+
+        for (int i = 0; i < n; i++) {
+            sb.append(pq.poll());
+            if (i != n - 1) sb.append("\n");
+        }
+
+        System.out.println(sb.toString());
+    }
+}
