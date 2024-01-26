12a1611cacddf20a316c0d6172d130073fd830da
B. Kim <121420533+lemon-lime-honey@users.noreply.github.com>
2024-01-26 17:11:14 +0900

    [Gold I] Title: 구슬 탈출 2, Time: 932 ms, Memory: 271400 KB -BaekjoonHub

---
+++ 백준/Gold/13460. 구슬 탈출 2/구슬 탈출 2.py
@@ -0,0 +1,66 @@
+from collections import deque
+import sys
+input = sys.stdin.readline
+
+
+def bfs():
+    que = deque()
+    chk = set()
+    que.append(tuple(balls))
+    chk.add(tuple(balls[:-1]))
+
+    while que:
+        br, bc, rr, rc, mv = que.popleft()
+        if mv >= 10:
+            return -1
+
+        for i in range(4):
+            nbr, nbc, bmv = move(br, bc, i)
+            nrr, nrc, rmv = move(rr, rc, i)
+
+            if board[nbr][nbc] != 'O':
+                if nrr == hole[0] and nrc == hole[1]:
+                    return mv + 1
+
+                if nbr == nrr and nbc == nrc:
+                    if bmv <= rmv:
+                        nrr, nrc = nrr - dr[i], nrc - dc[i]
+                    else:
+                        nbr, nbc = nbr - dr[i], nbc - dc[i]
+
+                if (nbr, nbc, nrr, nrc) in chk: continue
+                que.append((nbr, nbc, nrr, nrc, mv + 1))
+
+    return -1
+
+
+def move(r, c, idx):
+    mv = 0
+
+    while board[r + dr[idx]][c + dc[idx]] != '#' and board[r][c] != 'O':
+        r += dr[idx]
+        c += dc[idx]
+        mv += 1
+
+    return (r, c, mv)
+
+
+dr = [0, 0, -1, 1]
+dc = [1, -1, 0, 0]
+
+n, m = map(int, input().split())
+balls = [0, 0, 0, 0, 0]
+board = list()
+hole = [0, 0]
+
+for i in range(n):
+    board.append(input().rstrip())
+    for j in range(m):
+        if board[i][j] == 'O':
+            hole = [i, j]
+        elif board[i][j] == 'B':
+            balls[0: 2] = [i, j]
+        elif board[i][j] == 'R':
+            balls[2: 4] = [i, j]
+
+print(bfs())
