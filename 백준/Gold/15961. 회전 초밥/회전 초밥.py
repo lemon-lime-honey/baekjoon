c3ad95a53caa7ea8c54ded497e5875178c0ddf95
B. Kim <121420533+lemon-lime-honey@users.noreply.github.com>
2024-10-06 16:57:23 +0900

    [Gold IV] Title: 회전 초밥, Time: 688 ms, Memory: 165556 KB -BaekjoonHub

--- 백준/Gold/15961. 회전 초밥/회전 초밥.py
+++ 백준/Gold/15961. 회전 초밥/회전 초밥.py
@@ -1,43 +1,37 @@
-from collections import defaultdict
+from collections import deque
 import sys
+
 input = sys.stdin.readline
 
 n, d, k, c = map(int, input().split())
-food = [int(input()) for i in range(n)]
-pick = defaultdict(int)
-start, end = 0, k - 1
-types = set()
+dish = [int(input()) for i in range(n)]
+types = [0 for i in range(d + 1)]
+que = deque()
 result = 0
+cnt = 0
 
-for i in range(start, end + 1):
-    pick[food[i]] += 1
-    if pick[food[i]] == 1: types.add(food[i])
+for i in range(k):
+    que.append(dish[i])
+    if types[dish[i]] == 0:
+        cnt += 1
+    types[dish[i]] += 1
 
-start += 1
-end += 1
+result = cnt
 
-if end >= n:
-    if c not in types: result = len(types) + 1
-    else: result = len(types)
-    print(result)
-else:
-    while True:
-        pick[food[start - 1]] -= 1
-        pick[food[end]] += 1
+for i in range(n):
+    target = que.popleft()
+    types[target] -= 1
+    if types[target] == 0:
+        cnt -= 1
 
-        if pick[food[start - 1]] == 0:
-            types.remove(food[start - 1])
-        if pick[food[end]] == 1:
-            types.add(food[end])
-        if c not in types:
-            result = max(result, len(types) + 1)
-        else:
-            result = max(result, len(types))
+    que.append(dish[(i + k) % n])
+    if types[dish[(i + k) % n]] == 0:
+        cnt += 1
+    types[dish[(i + k) % n]] += 1
 
-        start += 1
-        end += 1
+    if types[c] == 0:
+        result = max(result, cnt + 1)
+    else:
+        result = max(result, cnt)
 
-        if end >= n: end = 0
-        if start >= n: break
-
-    print(result)
+print(result)
