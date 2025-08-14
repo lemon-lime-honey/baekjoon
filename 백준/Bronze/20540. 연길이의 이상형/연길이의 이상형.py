73b752ba1ea20afbee7aa554078395e89be8d2ea
B. Kim <121420533+lemon-lime-honey@users.noreply.github.com>
2025-08-15 01:48:15 +0900

    [Bronze III] Title: 연길이의 이상형, Time: 88 ms, Memory: 108384 KB -BaekjoonHub

---
+++ 백준/Bronze/20540. 연길이의 이상형/연길이의 이상형.py
@@ -0,0 +1,21 @@
+target = input()
+
+if target[0] == "E":
+    print("I", end="")
+else:
+    print("E", end="")
+
+if target[1] == "N":
+    print("S", end="")
+else:
+    print("N", end="")
+
+if target[2] == "F":
+    print("T", end="")
+else:
+    print("F", end="")
+
+if target[3] == "J":
+    print("P")
+else:
+    print("J")
