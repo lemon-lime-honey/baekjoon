ede2fdaf3d25469f3c3d845d9618e92400494633
B. Kim <121420533+lemon-lime-honey@users.noreply.github.com>
2025-10-06 22:16:27 +0900

    [Silver IV] Title: 게시판, Time: 148 ms, Memory: 110576 KB -BaekjoonHub

---
+++ 백준/Silver/3063. 게시판/게시판.py
@@ -0,0 +1,8 @@
+t = int(input())
+
+for i in range(t):
+    x1, y1, x2, y2, x3, y3, x4, y4 = map(int, input().split())
+    print(
+        (x2 - x1) * (y2 - y1)
+        - max(min(x2, x4) - max(x1, x3), 0) * max(min(y2, y4) - max(y1, y3), 0)
+    )
