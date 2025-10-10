bb12d2000751cbc00ef83dac30ba550de43e4520
B. Kim <121420533+lemon-lime-honey@users.noreply.github.com>
2025-10-11 00:01:43 +0900

    [Bronze IV] Title: Goodbye, Code Jam, Time: 92 ms, Memory: 108384 KB -BaekjoonHub

---
+++ 백준/Bronze/29738. Goodbye， Code Jam/Goodbye， Code Jam.py
@@ -0,0 +1,12 @@
+t = int(input())
+
+for i in range(1, t + 1):
+    n = int(input())
+    if n <= 25:
+        print(f"Case #{i}: World Finals")
+    elif n <= 1000:
+        print(f"Case #{i}: Round 3")
+    elif n <= 4500:
+        print(f"Case #{i}: Round 2")
+    else:
+        print(f"Case #{i}: Round 1")
