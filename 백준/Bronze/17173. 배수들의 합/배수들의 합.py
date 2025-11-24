c65567530eee091056565cdbff7ff2ff3a67435f
B. Kim <121420533+lemon-lime-honey@users.noreply.github.com>
2025-11-24 23:32:00 +0900

    [Bronze II] Title: 배수들의 합, Time: 96 ms, Memory: 109544 KB -BaekjoonHub

---
+++ 백준/Bronze/17173. 배수들의 합/배수들의 합.py
@@ -0,0 +1,12 @@
+n, m = map(int, input().split())
+nums = list(map(int, input().split()))
+
+result = 0
+
+for i in range(1, n + 1):
+    for num in nums:
+        if i % num == 0:
+            result += i
+            break
+
+print(result)
