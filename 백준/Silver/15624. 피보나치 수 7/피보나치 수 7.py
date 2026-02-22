c57d3a62f70a23f86d8bac2dff669b7a9be05ac7
B. Kim <121420533+lemon-lime-honey@users.noreply.github.com>
2026-02-22 11:08:52 +0900

    [Silver IV] Title: 피보나치 수 7, Time: 96 ms, Memory: 109544 KB, Score: 201 point -BaekjoonHub

--- 백준/Silver/15624. 피보나치 수 7/피보나치 수 7.py
+++ 백준/Silver/15624. 피보나치 수 7/피보나치 수 7.py
@@ -1,7 +1,11 @@
 n = int(input())
-nums = [0, 1]
 
-for i in range(n - 1):
-    nums[0], nums[1] = nums[1], (nums[0] + nums[1]) % 1_000_000_007
+if n == 0:
+    print(0)
+else:
+    nums = [0, 1]
 
-print(nums[1])
+    for i in range(n - 1):
+        nums[0], nums[1] = nums[1], (nums[0] + nums[1]) % 1_000_000_007
+
+    print(nums[1])
