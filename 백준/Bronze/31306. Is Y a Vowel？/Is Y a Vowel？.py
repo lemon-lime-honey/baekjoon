c08c3c91400a52748929e6f3b5eb4ad74e2e066c
B. Kim <121420533+lemon-lime-honey@users.noreply.github.com>
2026-03-04 05:42:56 +0900

    [Bronze IV] Title: Is Y a Vowel?, Time: 84 ms, Memory: 108384 KB -BaekjoonHub

---
+++ 백준/Bronze/31306. Is Y a Vowel？/Is Y a Vowel？.py
@@ -0,0 +1,12 @@
+ipt = input()
+
+a, e, i, o, u, y = (
+    ipt.count("a"),
+    ipt.count("e"),
+    ipt.count("i"),
+    ipt.count("o"),
+    ipt.count("u"),
+    ipt.count("y"),
+)
+
+print(a + e + i + o + u, a + e + i + o + u + y)
