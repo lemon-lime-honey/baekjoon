969893536f2a705bfe49bb57bd0f6dbec89ba00d
B. Kim <121420533+lemon-lime-honey@users.noreply.github.com>
2026-08-29 20:15:55 +0900

    [level 1] Title: 가운데 글자 가져오기, Time: 0.01 ms, Memory: 5.13 MB -BaekjoonHub

---
+++ 프로그래머스/1/12903. 가운데 글자 가져오기/가운데 글자 가져오기.cpp
@@ -0,0 +1,11 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+string solution(string s) {
+    if (s.length() % 2 == 0) {
+        return s.substr(s.length() / 2 - 1, 2);
+    }
+    return s.substr(s.length() / 2, 1);
+}
